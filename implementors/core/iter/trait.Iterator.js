78e9eae1111e54e7aa53995e97d724d6e5533dc1
Ty Overby <ty@pre-alpha.com>
2014-09-24 16:19:13 -0700

    add docs

---
+++ implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['curl'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;&amp;'a <a href='http://doc.rust-lang.org/master/std/primitive.str.html'>str</a>&gt; for <a class='struct' href='curl/struct.Protocols.html' title='curl::Protocols'>Protocols</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/curl/http/handle/trait.ToUrl.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['curl'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
