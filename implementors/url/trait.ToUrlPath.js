07ee96b0285910b6f72d3ed7f1142f1a2faca17e
Ty Overby <ty@pre-alpha.com>
2014-10-20 16:45:27 -0700

    update docs

--- implementors/url/trait.ToUrlPath.js
+++ implementors/url/trait.ToUrlPath.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors['url'] = [];
+implementors['hyper'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
